cdd7b9e32e47ff5b4a96da02d306599e52cd141e
just <just89@gmx.de>
2009-11-19 07:36:12 +0100

    fix strings

--- src/plugins/upload/lib/infojob.cpp
+++ src/plugins/upload/lib/infojob.cpp
@@ -104,18 +104,18 @@ void InfoJob::setState(const InfoJob::State &newState)
             break;
         }
     case InfoJob::Upload: {
-            emit description(this, i18n("YouTube"), qMakePair(i18n("Destination"),
+            emit description(this, i18n("Upload"), qMakePair(i18n("Destination"),
                                                              m_reply->request().url().toString()));
             emit infoMessage(this, i18n("Transmit Data"), QString());
             break;
         }
     case InfoJob::Download: {
-            emit description(this, i18n("YouTube"), qMakePair(i18n("Source"),
+            emit description(this, i18n("Upload"), qMakePair(i18n("Source"),
                                                              m_reply->request().url().toString()));
             break;
         }
     case InfoJob::Kill: {
-            emit description(this, i18n("YouTube"), qMakePair(i18n("Canceled"), QString()));
+            emit description(this, i18n("Upload"), qMakePair(i18n("Canceled"), QString()));
             break;
         }
     }
