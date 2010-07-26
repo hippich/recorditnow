9c73c033589593c40d6a038869e00d86c81210fd
Kai Dombrowe <just@suseDesktop.Kai>
2010-07-27 00:38:52 +0200

    set text for some keys

--- src/keyboard/keyloggerlabel.cpp
+++ src/keyboard/keyloggerlabel.cpp
@@ -39,6 +39,7 @@ KeyloggerLabel::KeyloggerLabel(QWidget *parent)
 {
 
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    setMaxLength(200);
 
 }
 
@@ -66,24 +67,75 @@ void KeyloggerLabel::releaseEvent(QKeyEvent *event)
 }
 
 
-static bool isModifier(const int &key)
-{
-
-    switch (key) {
-    case Qt::Key_Shift:
-    case Qt::Key_Alt:
-    case Qt::Key_Control:
-    case Qt::Key_Meta: return true;
-    default: return false;
-    }
-
-}
-
-
 void KeyloggerLabel::keyPressEvent(QKeyEvent *event)
 {
 
+    kDebug() << "key:" << event->key();
     m_keys.append(event->key());
+
+    QString txt;
+    switch (event->key()) {
+    case Qt::Key_Left: txt = i18nc("Keyboard key", "Left"); break;
+    case Qt::Key_Right: txt = i18nc("Keyboard key", "Right"); break;
+    case Qt::Key_Up: txt = i18nc("Keyboard key", "Up"); break;
+    case Qt::Key_Down: txt = i18nc("Keyboard key", "Down"); break;
+    case Qt::Key_Backspace: txt = i18nc("Keyboard key", "Backspace"); break;
+    case Qt::Key_Return: txt = i18nc("Keyboard key", "Return"); break;
+    case Qt::Key_Print: txt = i18nc("Keyboard key", "Print"); break;
+    case Qt::Key_Tab: txt = i18nc("Keyboard key", "Tab"); break;
+    case Qt::Key_Escape: txt = i18nc("Keyboard key", "Escape"); break;
+    case Qt::Key_F1: txt = "F1"; break;
+    case Qt::Key_F2: txt = "F2"; break;
+    case Qt::Key_F3: txt = "F3"; break;
+    case Qt::Key_F4: txt = "F4"; break;
+    case Qt::Key_F5: txt = "F5"; break;
+    case Qt::Key_F6: txt = "F6"; break;
+    case Qt::Key_F7: txt = "F7"; break;
+    case Qt::Key_F8: txt = "F8"; break;
+    case Qt::Key_F9: txt = "F9"; break;
+    case Qt::Key_F10: txt = "F10"; break;
+    case Qt::Key_F11: txt = "F11"; break;
+    case Qt::Key_F12: txt = "F12"; break;
+    case Qt::Key_F13: txt = "F13"; break;
+    case Qt::Key_F14: txt = "F14"; break;
+    case Qt::Key_F15: txt = "F15"; break;
+    case Qt::Key_F16: txt = "F16"; break;
+    case Qt::Key_F17: txt = "F17"; break;
+    case Qt::Key_F18: txt = "F18"; break;
+    case Qt::Key_F19: txt = "F19"; break;
+    case Qt::Key_F20: txt = "F20"; break;
+    case Qt::Key_F21: txt = "F21"; break;
+    case Qt::Key_F22: txt = "F22"; break;
+    case Qt::Key_F23: txt = "F23"; break;
+    case Qt::Key_F24: txt = "F24"; break;
+    case Qt::Key_F25: txt = "F25"; break;
+    case Qt::Key_F26: txt = "F26"; break;
+    case Qt::Key_F27: txt = "F27"; break;
+    case Qt::Key_F28: txt = "F28"; break;
+    case Qt::Key_F29: txt = "F29"; break;
+    case Qt::Key_F30: txt = "F30"; break;
+    case Qt::Key_F31: txt = "F31"; break;
+    case Qt::Key_F32: txt = "F32"; break;
+    case Qt::Key_F33: txt = "F33"; break;
+    case Qt::Key_F34: txt = "F34"; break;
+    case Qt::Key_F35: txt = "F35"; break;
+    case Qt::Key_Insert: txt = i18nc("Keyboard key", "Insert"); break;
+    case Qt::Key_End: txt = i18nc("Keyboard key", "End"); break;
+    case Qt::Key_PageUp: txt = i18nc("Keyboard key", "Page Up"); break;
+    case Qt::Key_PageDown: txt = i18nc("Keyboard key", "Page Down"); break;
+    case Qt::Key_Delete: txt = i18nc("Keyboard key", "Delete"); break;
+    case Qt::Key_NumLock: txt = i18nc("Keyboard key", "Num Lock"); break;
+    case Qt::Key_Space: txt = i18nc("Keyboard key", "Space"); break;
+    default: break;
+    }
+
+    if (!txt.isEmpty()) {
+        txt.prepend('(');
+        txt.append(')');
+        setText(text()+' '+txt);
+        return;
+    }
+
     QLineEdit::keyPressEvent(event);
     update();
 
